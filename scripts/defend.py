f3f6ce997136059184f4f3c798225d0aac19264a
giladcohen <giladadlig@gmail.com>
2020-04-29 03:42:09 +0300

    no message

--- scripts/defend.py
+++ scripts/defend.py
@@ -27,7 +27,7 @@ from art.attacks import FastGradientMethod, ProjectedGradientDescent, DeepFool, 
 from art.classifiers import PyTorchClassifier
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 adversarial robustness testing')
-parser.add_argument('--checkpoint_dir', default='/data/gilad/logs/adv_robustness/cifar100/resnet34/resnet34_00', type=str, help='checkpoint dir')
+parser.add_argument('--checkpoint_dir', default='/data/gilad/logs/adv_robustness/cifar10/resnet34/resnet34_00', type=str, help='checkpoint dir')
 parser.add_argument('--attack', default='fgsm', type=str, help='checkpoint dir')
 parser.add_argument('--targeted', default=True, type=boolean_string, help='use targeted attack')
 parser.add_argument('--attack_dir', default='', type=str, help='attack directory')
