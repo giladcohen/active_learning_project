dc5cf8e2b07ce6c8705a7a91c548da82fd134751
giladcohen <giladadlig@gmail.com>
2020-05-22 04:05:14 +0300

    no message

--- scripts/defend_2.py
+++ scripts/defend_2.py
@@ -24,18 +24,18 @@ import matplotlib.pyplot as plt
 from art.classifiers import PyTorchClassifier
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 adversarial robustness testing')
-parser.add_argument('--checkpoint_dir', default='/data/gilad/logs/adv_robustness/cifar10/resnet34/resnet34_00', type=str, help='checkpoint dir')
+parser.add_argument('--checkpoint_dir', default='/data/gilad/logs/adv_robustness/svhn/resnet34/resnet34_00', type=str, help='checkpoint dir')
 parser.add_argument('--attack', default='cw', type=str, help='checkpoint dir')
 parser.add_argument('--targeted', default=True, type=boolean_string, help='use targeted attack')
 parser.add_argument('--attack_dir', default='', type=str, help='attack directory')
 parser.add_argument('--rev_dir', default='fgsm_minimal', type=str, help='reverse dir')
 parser.add_argument('--batch_size', default=100, type=int, help='batch size')
-parser.add_argument('--method', default='cross_inference_svm_v2', type=str, help='method of defense: ensemble, svm')
+parser.add_argument('--method', default='ensemble', type=str, help='method of defense: ensemble, smart_ensemble, svm, cross_inference_svm')
 parser.add_argument('--pool', default='all', type=str, help='networks pool: main, ensemble, all')
 parser.add_argument('--train_on', default='adv', type=str, help='normal, adv, all')
 parser.add_argument('--test_on', default='adv', type=str, help='normal, adv')
-parser.add_argument('--temperature', default=4, type=float, help='normal, adv')
-parser.add_argument('--pca_dims', default=50, type=int, help='if not -1, apply PCA to svm with dims')
+parser.add_argument('--temperature', default=1, type=float, help='normal, adv')
+parser.add_argument('--pca_dims', default=-1, type=int, help='if not -1, apply PCA to svm with dims')
 
 parser.add_argument('--mode', default='null', type=str, help='to bypass pycharm bug')
 parser.add_argument('--port', default='null', type=str, help='to bypass pycharm bug')
@@ -452,7 +452,7 @@ elif 'svm' in args.method:
         input_features = pca.transform(input_features)
         test_features  = pca.transform(test_features)
 
-    clf = LinearSVC(penalty='l2', loss='hinge', verbose=1, random_state=rand_gen, max_iter=100000)
+    clf = LinearSVC(penalty='l2', loss='hinge', verbose=1, random_state=rand_gen, max_iter=10000)
     clf.fit(input_features, input_labels)
     defense_preds = clf.predict(test_features)
 
