bbb484e6735e704d22bbe18e12ea241d3c09553f
giladcohen <giladadlig@gmail.com>
2021-02-02 13:08:30 +0200

    no message

--- scripts/defend_tta.py
+++ scripts/defend_tta.py
@@ -31,8 +31,8 @@ from art.classifiers import PyTorchClassifier
 from active_learning_project.classifiers.pytorch_ext_classifier import PyTorchExtClassifier
 
 parser = argparse.ArgumentParser(description='PyTorch adversarial robustness testing')
-parser.add_argument('--checkpoint_dir', default='/data/gilad/logs/adv_robustness/cifar100/resnet34/regular/resnet34_00', type=str, help='checkpoint dir')
-parser.add_argument('--attack_dir', default='fgsm_targeted', type=str, help='attack directory')
+parser.add_argument('--checkpoint_dir', default='/data/gilad/logs/adv_robustness/svhn/resnet34/regular/resnet34_00', type=str, help='checkpoint dir')
+parser.add_argument('--attack_dir', default='deepfool', type=str, help='attack directory')
 parser.add_argument('--save_dir', default='tta_ball_rev_L2_eps_2_n_1000', type=str, help='reverse dir')
 parser.add_argument('--batch_size', default=100, type=int, help='batch size')
 parser.add_argument('--subset', default=-1, type=int, help='attack only subset of test set')
