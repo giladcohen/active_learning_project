a17294b715057e74d98ea920fade300808a1ec70
giladcohen <giladadlig@gmail.com>
2021-07-02 04:28:07 +0300

    updating pointnet training with two tasks

--- scripts/defend_tta_with_pointnet.py
+++ scripts/defend_tta_with_pointnet.py
@@ -1,6 +1,6 @@
 """
-This script uses TTA for robustness. Each TTA is perturbed to maximize the KL divergence between the TTA and
-the original example.
+This script uses TTA for robustness and adv classification.
+We pass the network's features into a pointnet which classifies the truth label, and whether or not the image is adv.
 """
 from typing import Tuple
 import torch
@@ -46,8 +46,7 @@ parser.add_argument('--checkpoint_dir',
 parser.add_argument('--attack_dir', default='cw_targeted', type=str, help='attack directory')
 
 # eval:
-parser.add_argument('--tta_size', default=1000, type=int, help='number of test-time augmentations')
-parser.add_argument('--eval_batch_size', default=100, type=int, help='batch size for the TTA evaluation')
+parser.add_argument('--tta_size', default=100, type=int, help='number of test-time augmentations')
 
 # transforms:
 parser.add_argument('--clip_inputs', action='store_true', help='clipping TTA inputs between 0 and 1')
@@ -58,9 +57,10 @@ parser.add_argument('--soft_transforms', action='store_true', help='applying mel
 parser.add_argument('--train_batch_size', default=128, type=int, help='batch size for the TTA training')
 parser.add_argument('--steps', default=2000, type=int, help='training steps for each image')
 parser.add_argument('--ema_decay', default=0.998, type=float, help='EMA decay')
-parser.add_argument('--val_size', default=None, type=int, help='validation size')
-parser.add_argument('--lambda_feat_trans', default=0.0, type=float, help='validation size')
-parser.add_argument('--num_workers', default=4, type=int, help='number of workers')
+parser.add_argument('--val_size', default=100, type=int, help='validation size')
+parser.add_argument('--lambda_feat_trans', default=0.0, type=float, help='regularization on ')
+parser.add_argument('--lambda_adv_det', default=1.0, type=float, help='adversarial detection coefficient')
+parser.add_argument('--num_workers', default=0, type=int, help='number of workers')
 
 # architecture
 parser.add_argument('--features', default='probs', type=str, help='which features to use from resnet: embeddings/logits/probs')
@@ -81,10 +81,6 @@ parser.add_argument('--port', default='null', type=str, help='to bypass pycharm 
 
 args = parser.parse_args()
 
-MaxInt32 =  1 << 32 - 1
-
-TRAIN_TIME_CNT = 0.0
-TEST_TIME_CNT = 0.0
 ATTACK_DIR = os.path.join(args.checkpoint_dir, args.attack_dir)
 DUMP_DIR = os.path.join(ATTACK_DIR, args.dump_dir)
 os.makedirs(DUMP_DIR, exist_ok=True)
@@ -111,14 +107,14 @@ CHECKPOINT_PATH = os.path.join(args.checkpoint_dir, 'ckpt.pth')
 
 rand_gen = np.random.RandomState(12345)
 
-train_adv_det_writer = SummaryWriter(os.path.join(DUMP_DIR, 'train_adv_detection'))
-test_adv_det_writer = SummaryWriter(os.path.join(DUMP_DIR, 'test_adv_detection'))
+train_defense_writer = SummaryWriter(os.path.join(DUMP_DIR, 'train_adv_detection'))
+test_defense_writer = SummaryWriter(os.path.join(DUMP_DIR, 'test_adv_detection'))
 
 # Data
 logger.info('==> Preparing data..')
 all_dataset_eval_loader = get_test_loader(
     dataset=dataset,
-    batch_size=args.eval_batch_size,
+    batch_size=100,
     num_workers=1,
     pin_memory=True
 )
@@ -183,7 +179,7 @@ elif args.features == 'embeddings':
 else:
     AssertionError('Unexpected args.features={}'.format(args.features))
 
-pointnet = PointNet(k=1, channel=num_channels)
+pointnet = PointNet(k=len(classes) + 1, channel=num_channels)
 pointnet = pointnet.to(device)
 
 # summary(net, (3, 32, 32))
@@ -191,64 +187,62 @@ if device == 'cuda':
     # net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
-def reset_opt():
-    global optimizer
-    if args.opt == 'sgd':
-        optimizer = optim.SGD(
-            pointnet.parameters(),
-            lr=args.lr,
-            momentum=args.mom,
-            weight_decay=args.wd,
-            nesterov=args.mom > 0)
-    elif args.opt == 'adam':
-        optimizer = optim.Adam(
-            pointnet.parameters(),
-            lr=args.lr,
-            betas=(args.mom, 0.999),
-            weight_decay=args.wd)
-    elif args.opt == 'adamw':
-        optimizer = optim.AdamW(
-            pointnet.parameters(),
-            lr=args.lr,
-            betas=(args.mom, 0.999),
-            weight_decay=args.wd)
-    elif args.opt == 'rmsprop':
-        optimizer = optim.RMSprop(
-            pointnet.parameters(),
-            lr=args.lr,
-            momentum=args.mom,
-            weight_decay=args.wd)
-    else:
-        raise AssertionError('optimizer {} is not expected'.format(args.opt))
+if args.opt == 'sgd':
+    optimizer = optim.SGD(
+        pointnet.parameters(),
+        lr=args.lr,
+        momentum=args.mom,
+        weight_decay=args.wd,
+        nesterov=args.mom > 0)
+elif args.opt == 'adam':
+    optimizer = optim.Adam(
+        pointnet.parameters(),
+        lr=args.lr,
+        betas=(args.mom, 0.999),
+        weight_decay=args.wd)
+elif args.opt == 'adamw':
+    optimizer = optim.AdamW(
+        pointnet.parameters(),
+        lr=args.lr,
+        betas=(args.mom, 0.999),
+        weight_decay=args.wd)
+elif args.opt == 'rmsprop':
+    optimizer = optim.RMSprop(
+        pointnet.parameters(),
+        lr=args.lr,
+        momentum=args.mom,
+        weight_decay=args.wd)
+else:
+    raise AssertionError('optimizer {} is not expected'.format(args.opt))
 
 def flush():
-    train_adv_det_writer.flush()
+    train_defense_writer.flush()
 
 
-reset_opt()
 bce_loss = nn.BCEWithLogitsLoss()
+ce_loss = nn.CrossEntropyLoss()
 
 tta_dataset_train = TTADataset(
     torch.from_numpy(X_test[val_inds]),
     torch.from_numpy(X_test_adv[val_inds]),
+    torch.from_numpy(y_test[val_inds]),
     args.tta_size,
     transform=tta_transforms)
 
 tta_dataset_test = TTADataset(
     torch.from_numpy(X_test[test_inds]),
     torch.from_numpy(X_test_adv[test_inds]),
+    torch.from_numpy(y_test[test_inds]),
     args.tta_size,
     transform=tta_transforms)
 
 train_loader = torch.utils.data.DataLoader(
     tta_dataset_train, batch_size=1, shuffle=True,
-    num_workers=args.num_workers, pin_memory=device=='cuda'
-)
+    num_workers=args.num_workers, pin_memory=device=='cuda')
 
 test_loader = torch.utils.data.DataLoader(
     tta_dataset_test, batch_size=1, shuffle=False,
-    num_workers=args.num_workers, pin_memory=device=='cuda'
-)
+    num_workers=args.num_workers, pin_memory=device=='cuda')
 
 def rearrange_as_pts(x: torch.Tensor) -> torch.Tensor:
     """Reshape the x tensor from [B * N, D] to [B, D, N]"""
@@ -257,118 +251,142 @@ def rearrange_as_pts(x: torch.Tensor) -> torch.Tensor:
     return x
 
 def train():
-    global global_step, total_loss
+    global global_step
     net.eval()
     pointnet.train()
     optimizer.zero_grad()
     start_time = time.time()
 
     net_features = np.nan * torch.ones(args.train_batch_size, num_channels, args.tta_size).to(device)  # (B, D, N)
-    y = np.nan * torch.ones(args.train_batch_size).to(device)  # B
+    y_gt = np.nan * torch.ones(args.train_batch_size).to(device)  # B
+    y_is_adv_gt = np.nan * torch.ones(args.train_batch_size).to(device)  # B
 
     batch_cnt = 0
     while batch_cnt < args.train_batch_size:
-        for batch_idx, (inputs, targets) in enumerate(train_loader):
-            targets = targets.reshape(-1)
+        for batch_idx, (x, yb, yb_is_adv) in enumerate(train_loader):
+            x, yb, yb_is_adv = x.reshape((-1,) + img_shape), yb.reshape(-1), yb_is_adv.reshape(-1)
+            x, yb, yb_is_adv = x.to(device), yb.to(device), yb_is_adv.to(device)
             b = batch_cnt
-            e = b + targets.size(0)
-            inputs = inputs.reshape((-1,) + img_shape)
-            assert not inputs.isnan().any()
-            inputs, targets = inputs.to(device), targets.to(device)
+            e = b + yb.size(0)
+            assert not x.isnan().any()
             with torch.no_grad():
-                net_features[b:e] = rearrange_as_pts(net(inputs)[args.features])
-            y[b:e] = targets
-            batch_cnt += targets.size(0)
+                net_features[b:e] = rearrange_as_pts(net(x)[args.features])
+            y_gt[b:e] = yb
+            y_is_adv_gt[b:e] = yb_is_adv
+            batch_cnt += yb.size(0)
 
             if batch_cnt >= args.train_batch_size:
                 break
 
     assert batch_cnt == args.train_batch_size
     assert not net_features.isnan().any()
-    assert not y.isnan().any()
+    assert not y_gt.isnan().any()
+    assert not y_is_adv_gt.isnan().any()
 
     out, trans_feat = pointnet(net_features)
-    out = out.squeeze()
-    loss_bce = bce_loss(out, y)
+    y_probs = out[:, :len(classes)]
+    y_is_adv_probs = out[:, -1]
+    loss_ce = ce_loss(y_probs, y_gt.long())
+    loss_bce = bce_loss(y_is_adv_probs, y_is_adv_gt)
     loss_feat_trans = feature_transform_reguliarzer(trans_feat)
-    loss = loss_bce + args.lambda_feat_trans * loss_feat_trans
+    loss = loss_ce + args.lambda_adv_det * loss_bce + args.lambda_feat_trans * loss_feat_trans
 
     loss.backward()
     optimizer.step()
 
-    preds = out.ge(0.0)
-    num_corrected = preds.eq(y).sum().item()
-    acc = num_corrected / y.size(0)
+    y_preds = y_probs.argmax(dim=1)
+    y_is_adv_preds = y_is_adv_probs.ge(0.0)
+    num_corrected_classification = y_preds.eq(y_gt).sum().item()
+    num_corrected_is_adv = y_is_adv_preds.eq(y_is_adv_gt).sum().item()
+    classification_acc = num_corrected_classification / y_gt.size(0)
+    is_adv_acc = num_corrected_is_adv / y_gt.size(0)
     end_time = time.time()
 
     if global_step % 1 == 0:  # sampling, once ever 1 train iterations
-        train_adv_det_writer.add_scalar('losses/loss_bce', loss_bce, global_step)
-        train_adv_det_writer.add_scalar('losses/loss_feat_trans', loss_feat_trans, global_step)
-        train_adv_det_writer.add_scalar('losses/loss', loss, global_step)
-        train_adv_det_writer.add_scalar('metrics/acc', 100.0 * acc, global_step)
-        train_adv_det_writer.add_scalar('stats/secs_per_image', (end_time - start_time)/args.train_batch_size, global_step)
+        train_defense_writer.add_scalar('losses/loss_ce', loss_bce, global_step)
+        train_defense_writer.add_scalar('losses/loss_bce', loss_bce, global_step)
+        train_defense_writer.add_scalar('losses/loss_feat_trans', loss_feat_trans, global_step)
+        train_defense_writer.add_scalar('losses/loss', loss, global_step)
+        train_defense_writer.add_scalar('metrics/classification_acc', 100.0 * classification_acc, global_step)
+        train_defense_writer.add_scalar('metrics/is_adv_acc', 100.0 * is_adv_acc, global_step)
+        train_defense_writer.add_scalar('stats/secs_per_image', (end_time - start_time) / args.train_batch_size, global_step)
 
-    logger.info('global step #{} (TRAIN): loss={}\tacc={:.4f}'.format(global_step + 1, loss, 100.0 * acc))
+    logger.info('global step #{} (TRAIN): loss={}\tclassification acc={:.4f}, is adv acc={:.4f}'
+                .format(global_step + 1, loss, 100.0 * classification_acc, 100.0 * is_adv_acc))
 
 def test():
-    # start with the normal
     start_time = time.time()
     net.eval()
     pointnet.eval()
-    all_preds = np.nan * np.ones(test_size)
-    all_gt = np.nan * np.ones(test_size)
+    y_preds = np.nan * np.ones(2 * test_size)
+    y_is_adv_preds = np.nan * np.ones(2 * test_size)
+    y_gt = np.nan * np.ones(2 * test_size)
+    y_is_adv_gt = np.nan * np.ones(2 * test_size)
 
     all_cnt = 0
     loss = 0.0
+    loss_ce = 0.0
     loss_bce = 0.0
     loss_feat_trans = 0.0
 
-    while all_cnt < test_size:
-        for batch_idx, (inputs, targets) in enumerate(test_loader):
-            y = targets.reshape(-1)
-            b = all_cnt
-            e = b + y.size(0)
-            inputs = inputs.reshape((-1,) + img_shape)
-            assert not inputs.isnan().any()
-            inputs, y = inputs.to(device), y.to(device)
-            net_features = rearrange_as_pts(net(inputs)[args.features])
-            out, trans_feat = pointnet(net_features)
-            out = out.squeeze()
-            loss_bce += bce_loss(out, y)
-            loss_feat_trans += feature_transform_reguliarzer(trans_feat)
-            loss += loss_bce + args.lambda_feat_trans * loss_feat_trans
+    for batch_idx, (x, yb, yb_is_adv) in enumerate(test_loader):
+        x, yb, yb_is_adv = x.reshape((-1,) + img_shape), yb.reshape(-1), yb_is_adv.reshape(-1)
+        x, yb, yb_is_adv = x.to(device), yb.to(device), yb_is_adv.to(device)
+        b = all_cnt
+        e = b + yb.size(0)
+        assert not x.isnan().any()
+        net_features = rearrange_as_pts(net(x)[args.features])
+        out, trans_feat = pointnet(net_features)
+        y_probs = out[:, :len(classes)]
+        y_is_adv_probs = out[:, -1]
 
-            preds = out.ge(0.0)
-            all_preds[b:e] = preds.cpu().numpy()
-            all_gt[b:e] = y.cpu().numpy()
+        loss_ce += ce_loss(y_probs, yb)
+        loss_bce += bce_loss(y_is_adv_probs, yb_is_adv)
+        loss_feat_trans += feature_transform_reguliarzer(trans_feat)
+        loss = loss_ce + args.lambda_adv_det * loss_bce + args.lambda_feat_trans * loss_feat_trans
 
-            all_cnt += y.size(0)
-            if all_cnt >= test_size:
-                break
+        y_preds[b:e] = y_probs.argmax(dim=1).cpu().numpy()
+        y_is_adv_preds[b:e] = y_is_adv_probs.ge(0.0).cpu().numpy()
+        y_gt[b:e] = yb.cpu().numpy()
+        y_is_adv_gt[b:e] = yb_is_adv.cpu().numpy()
 
-    assert all_cnt == test_size
-    assert not (np.isnan(all_preds)).any()
-    assert not (np.isnan(all_gt)).any()
+        all_cnt += yb.size(0)
+
+    assert all_cnt == 2 * test_size
+    assert not (np.isnan(y_preds)).any()
+    assert not (np.isnan(y_is_adv_preds)).any()
+    assert not (np.isnan(y_gt)).any()
+    assert not (np.isnan(y_is_adv_gt)).any()
 
     # averaging losses
-    loss /= test_size
-    loss_feat_trans /= test_size
-    loss_bce /= test_size
+    loss /= (2 * test_size)
+    loss_ce /= (2 * test_size)
+    loss_feat_trans /= (2 * test_size)
+    loss_bce /= (2 * test_size)
 
-    acc = np.mean(all_preds == all_gt)
-    all_normal_indices = np.where(all_gt == 0)[0]
-    all_adv_indices = np.where(all_gt == 1)[0]
-    norm_acc = np.mean(all_preds[all_normal_indices] == all_gt[all_normal_indices])
-    adv_acc = np.mean(all_preds[all_adv_indices] == all_gt[all_adv_indices])
+    classification_acc = np.mean(y_preds == y_gt)
+    is_adv_acc = np.mean(y_is_adv_preds == y_is_adv_gt)
+
+    all_normal_indices = np.where(y_is_adv_gt == 0)[0]
+    all_adv_indices = np.where(y_is_adv_gt == 1)[0]
+    norm_classification_acc = np.mean(y_preds[all_normal_indices] == y_gt[all_normal_indices])
+    adv_classification_acc = np.mean(y_preds[all_adv_indices] == y_gt[all_adv_indices])
+    norm_is_adv_acc = np.mean(y_is_adv_preds[all_normal_indices] == y_is_adv_gt[all_normal_indices])
+    adv_is_adv_acc = np.mean(y_is_adv_preds[all_adv_indices] == y_is_adv_gt[all_adv_indices])
+
     end_time = time.time()
 
-    test_adv_det_writer.add_scalar('losses/loss_bce', loss_bce, global_step)
-    test_adv_det_writer.add_scalar('losses/loss_feat_trans', loss_feat_trans, global_step)
-    test_adv_det_writer.add_scalar('losses/loss', loss, global_step)
-    test_adv_det_writer.add_scalar('metrics/acc', 100.0 * acc, global_step)
-    test_adv_det_writer.add_scalar('metrics/normal_acc', 100.0 * norm_acc, global_step)
-    test_adv_det_writer.add_scalar('metrics/adv_acc', 100.0 * adv_acc, global_step)
-    test_adv_det_writer.add_scalar('stats/secs_per_image', (end_time - start_time)/test_size, global_step)
+    test_defense_writer.add_scalar('losses/loss_ce', loss_ce, global_step)
+    test_defense_writer.add_scalar('losses/loss_bce', loss_bce, global_step)
+    test_defense_writer.add_scalar('losses/loss_feat_trans', loss_feat_trans, global_step)
+    test_defense_writer.add_scalar('losses/loss', loss, global_step)
+    test_defense_writer.add_scalar('metrics/classification_acc', 100.0 * classification_acc, global_step)
+    test_defense_writer.add_scalar('metrics/is_adv_acc', 100.0 * is_adv_acc, global_step)
+    test_defense_writer.add_scalar('metrics/norm_classification_acc', 100.0 * norm_classification_acc, global_step)
+    test_defense_writer.add_scalar('metrics/adv_classification_acc', 100.0 * adv_classification_acc, global_step)
+    test_defense_writer.add_scalar('metrics/norm_is_adv_acc', 100.0 * norm_is_adv_acc, global_step)
+    test_defense_writer.add_scalar('metrics/adv_is_adv_acc', 100.0 * adv_is_adv_acc, global_step)
+    test_defense_writer.add_scalar('stats/secs_per_image', (end_time - start_time) / test_size, global_step)
 
     logger.info('global step #{} (TEST): loss={}\tacc={:.4f}'.format(global_step + 1, loss, 100.0 * acc))
 
