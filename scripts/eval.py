f5f49490ec607df65a970dabdc9cf7e05ccc4591
giladcohen <giladadlig@gmail.com>
2021-08-22 20:49:17 +0300

    remove settree

--- scripts/eval.py
+++ scripts/eval.py
@@ -19,7 +19,6 @@ from sklearn.ensemble import RandomForestClassifier
 
 sys.path.insert(0, ".")
 sys.path.insert(0, "./adversarial_robustness_toolbox")
-sys.path.insert(0, "./Set_Tree")
 
 from active_learning_project.datasets.train_val_test_data_loaders import get_test_loader, get_train_valid_loader, \
     get_loader_with_specific_inds, get_normalized_tensor
@@ -29,13 +28,12 @@ from active_learning_project.utils import boolean_string, pytorch_evaluate, set_
     majority_vote, convert_tensor_to_image, print_Linf_dists, calc_attack_rate, get_image_shape
 from active_learning_project.models.utils import get_strides, get_conv1_params, get_model
 from art.classifiers import PyTorchClassifier
-import settree
 
 parser = argparse.ArgumentParser(description='Evaluating robustness score')
-parser.add_argument('--checkpoint_dir', default='/data/gilad/logs/adv_robustness/cifar10/resnet34/regular/resnet34_00', type=str, help='checkpoint dir')
+parser.add_argument('--checkpoint_dir', default='/data/gilad/logs/adv_robustness/tiny_imagenet/resnet50/adv_robust_trades', type=str, help='checkpoint dir')
 parser.add_argument('--checkpoint_file', default='ckpt.pth', type=str, help='checkpoint path file name')
-parser.add_argument('--method', default='simple', type=str, help='simple, ensemble, tta, random_forest, random_forest_sets')
-parser.add_argument('--attack_dir', default='', type=str, help='attack directory, or None for normal images')
+parser.add_argument('--method', default='random_forest', type=str, help='simple, ensemble, tta, random_forest, random_forest_sets')
+parser.add_argument('--attack_dir', default='jsma_targeted', type=str, help='attack directory, or None for normal images')
 parser.add_argument('--batch_size', default=100, type=int, help='batch size')
 
 # tta method params:
@@ -45,7 +43,7 @@ parser.add_argument('--tta_output_dir', default='tta', type=str, help='The dir t
 parser.add_argument('--soft_transforms', action='store_true', help='applying mellow transforms')
 parser.add_argument('--clip_inputs', action='store_true', help='clipping TTA inputs between 0 and 1')
 parser.add_argument('--overwrite', action='store_true', help='force calculating and saving TTA')
-parser.add_argument('--num_workers', default=4, type=int, help='Data loading threads for tta loader')
+parser.add_argument('--num_workers', default=20, type=int, help='Data loading threads for tta loader or random forest')
 
 # dump
 parser.add_argument('--dump_dir', default=None, type=str, help='dump dir for logs and data')
@@ -216,27 +214,6 @@ elif 'random_forest' in args.method:
         cls_rf.fit(features_train, labels_train)
         y_preds_norm = cls_rf.predict(features_test_norm)
         y_preds = cls_rf.predict(features_test_adv)
-
-    elif args.method == 'random_forest_sets':
-        # concatenate features:
-        features_train = np.concatenate((tta_logits_train_norm, tta_logits_train_adv), axis=0)
-        labels_train = np.concatenate((y_test[val_inds], y_test[val_inds]), axis=0)
-
-        set_data_train = settree.SetDataset(records=[features_train[i] for i in range(len(features_train))])
-        set_data_test_norm = settree.SetDataset(records=[tta_logits_test_norm[i] for i in range(len(tta_logits_test_norm))])
-        set_data_test_adv = settree.SetDataset(records=[tta_logits_test_adv[i] for i in range(len(tta_logits_test_adv))])
-
-        set_tree_model = settree.SetTree(classifier=True,
-                                         criterion='gini',
-                                         operations=settree.OPERATIONS,
-                                         use_attention_set=True,
-                                         use_attention_set_comp=True,
-                                         attention_set_limit=5,
-                                         max_depth=None,
-                                         random_state=rand_gen)
-        set_tree_model.fit(set_data_train, labels_train)
-        y_preds_norm = set_tree_model.predict(set_data_test_norm)
-        y_preds = set_tree_model.predict(set_data_test_adv)
     else:
         raise AssertionError('unknown method {}'.format(args.method))
 
